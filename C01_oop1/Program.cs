80f7d39b4c7c80dda0fbe8d40e6979f2aa4c0b9a
Moho32bit00 <mohamed.man2007@gmail.com>
2026-09-09 19:47:58 +0300

    Part02 - 1)

--- C01_oop1/Program.cs
+++ C01_oop1/Program.cs
@@ -2,6 +2,30 @@
 {
     internal class Program
     {
+        #region DeliveryAddress
+        public struct DeliveryAddress
+        {
+            public string City { set; get; }
+            public string Street { set; get; }
+            public int BuildingNumber { set; get; }
+
+                
+
+
+            DeliveryAddress(string city , string Street, int buildingNumber)
+            {
+                this.City = city;
+                this.Street = Street;
+                this.BuildingNumber = buildingNumber;
+            }
+
+            public string GetFullAddress()
+            {
+                return $"City : {City}\nStreet : {Street}\nBuilding Number : {BuildingNumber}\n";
+            }
+        }
+        #endregion 
+
         static void Main(string[] args)
         {
 
@@ -13,21 +37,32 @@
             //b) the other variable well get the reference value from the Customer Variable cuz we are here dealinmg with a class so anything
             //affect the other variable will also affect the class variable . 
 
-            #endregion  
+            #endregion
 
 
             #region q2 
             //a) 1- the access modifiers that are used is punlic .
-               //2- there is no validation for the user input .
-               //3- there is no properties to deal with the fielsd through (seters , geters) .
+            //2- there is no validation for the user input .
+            //3- there is no properties to deal with the fielsd through (seters , geters) .
 
 
             //b) it will gave a limited access to the user on those fields .
-            #endregion 
+            #endregion
 
 
             #endregion
 
+
+            #region Part 02 : Practical
+            #region 1) 
+            //DeliveryAddress d1 = new DeliveryAddress();
+            //d1.Street = "63-St";
+            //string str = d1.Street;
+            //str = "45-str";
+            //Console.WriteLine(d1.Street);
+            //Console.WriteLine(str);
+            #endregion
+            #endregion
         }
     }
 }
