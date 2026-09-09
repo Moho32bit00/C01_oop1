f0d41cc40fc79454e7c096ce8f32e54191161cbb
Moho32bit00 <mohamed.man2007@gmail.com>
2026-09-09 19:21:58 +0300

    Q1 answer

--- C01_oop1/Program.cs
+++ C01_oop1/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Part 01 : Theoretical Questions
+
+            #region q1 
+            //a) well the other variable will get the copy of value from the DeliveryAddress variable cuz we are here dealing with a struct 
+            
+            //b) the other variable well get the reference value from the Customer Variable cuz we are here dealinmg with a class so anything
+            //affect the other variable will also affect the class variable . 
+
+            #endregion 
+
+
+
+
+
+            #endregion
+
         }
     }
 }
